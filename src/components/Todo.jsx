0c7ad3e5c3eff112780e7183839dc16bd745ab2f
kajalRani22 <annatomar6033@gmail.com>
2024-12-26 14:32:16 +0530

    second commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,32 +1,25 @@
 
 import { MdDeleteForever } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { addTask, deleteTask, fetchTask } from "../store";
-import { useState } from "react";
+import {useState} from "react"
+import { addTask, deleteTask, clearTask } from "../store";
+
 const Todo = () => {
-    const[task, setTask]= useState("");
-  const tasks = useSelector((state) => state.task);
-
-
+    const[userTask, setUserTask]= useState("");
+    
+  const tasks= useSelector((state) => state.taskReducer.task);
   const dispatch = useDispatch();
-  console.log(tasks);
-  
-
-  //handle delt 
-  const handleTaskDelete=(id)=>{
-    dispatch(deleteTask(id));
-   
-
+  const handleFormSubmit = (e)=>{
+   e.preventDefault();
+   dispatch(addTask(userTask));
+   setUserTask("");
   };
-  const handleFetchTasks=()=>{
-dispatch(fetchTask())
-  } 
-
-  const handleFormSubmit= (e)=>{
-    e.preventDefault();
-    if(task.trim())
-      dispatch(addTask(task));
-       setTask("");
+  const handleDelete=(index)=>{
+    dispatch(deleteTask(index))
+  }
+  const allClear = ()=>{
+    dispatch(clearTask());
+    
   }
   return (
     <div className="container">
@@ -36,27 +29,22 @@ dispatch(fetchTask())
         </h1>
         <div className="row">
           <form onSubmit={handleFormSubmit}>
-            <input type="text" id="input-box" placeholder="Add a new task"  value={task} onChange={(e)=>setTask(e.target.value)}/>
-            <button>Add Task</button>
+            <input type="text" id="input-box" placeholder="Add a new task"  value={userTask} onChange={(e)=>setUserTask(e.target.value)}/>
+            <button type="submit">Add Task</button>
           </form>
         </div>
-        <button onClick={handleFetchTasks}>Fetch Tasks
-             
-        </button>
+        <button type="reset" onClick={()=>allClear()}>All clear</button>
+        
         <ul id="list-container">
-          {" "}
-          {/* data display ho jayega screen pr*/}
-          {tasks.map((curTask, index) => {
-            return (
+        {tasks?.map((curTask, index) => {
+            return(
               <li key={index}>
                 <p>
                   {index}: {curTask}
                 </p>
                 <div>
                   <MdDeleteForever
-                    className="icon-style"
-                    onClick={() => handleTaskDelete(index)}
-                  />
+                    className="icon-style" onClick={()=>handleDelete(index)}/>
                 </div>
               </li>
             );
--- src/store.jsx
+++ src/store.jsx
@@ -1,80 +1,44 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { thunk } from "redux-thunk";
-const ADD_TASK = "task/add";
-const DELETE_TASK = "task/delete";
-const  FETCH_TASKS = "task/fetch";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   task: [],
-  isLoading: false,
 };
-
-const taskReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TASK:
-      return {
-        ...state,
-        task: [...state.task, action.payload],
-      };
-    case DELETE_TASK:
-      // eslint-disable-next-line no-case-declarations
-      const updatedTask = state.task.filter((curTask, index) => {
-        return index !==action.payload;
-      });
-      return {
-        ...state,
-        task: updatedTask,
-      };
-
-
-      case FETCH_TASKS:
-        return{
-      ...state,
-      task:[...state.task, ...action.payload],
-
-        };
-    default:
-      return state;
-  }
-};
-
-//step2: Create the Redux store usinsg sthe reducer..
-export const store = createStore(taskReducer, composeWithDevTools(applyMiddleware)(thunk));
-console.log(store);
-
-console.log("initial State:", store.getState());
-
-//step5: Create action creators
-export const addTask = (data) => {
-  return { type: ADD_TASK, payload: data };
-};
- export const deleteTask = (id) => {
-  return { type: DELETE_TASK, payload: id };
-};
-
-//Step4: Dispatch an action to add a task
-store.dispatch(addTask("buy bottle"));
-store.dispatch(addTask("buy phn"));
-store.dispatch(addTask("buy earphone"));
-console.log("updated State:", store.getState())
-//store dispatch ({type: ADD_TASK, payload: "Buy Mango"})
-store.dispatch(addTask("Buy Mango"));
-console.log("updated State:", store.getState());
-//store.dispatch({type: DELETE_TASK, payload: 1})
-store.dispatch(deleteTask(1));
-console.log("deleted State:", store.getState());
-export const fetchTask = ()=>{
-    return async (dispatch)=>{
-try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
-    const task = await res.json();
-    console.log(task);
-    dispatch({type: FETCH_TASKS, payload: task.map((curTask)=> curTask.title) })
-} catch (error) {
-    console.log(error)
-}
+//Redux Toolkit slice
+const taskReducer = createSlice({
+  name: "task",
+  initialState,
+  reducers: {
+    addTask(state, action) {
+state.task.push(action.payload);
+//state.task=[...state.task, action.payload],
+    },
+    deleteTask(state, action) {
+      state.task = state.task.filter(
+        (curTask, index) => index !== action.payload
+      );
+    },
+    clearTask() {
+      return [];  // Completely resets state to an empty array
     }
-}
+  },
+});
+console.log(taskReducer);
+export const { addTask, deleteTask, clearTask } = taskReducer.actions;
 
+//(New style)
+export const store = configureStore({
+  reducer: {
+    taskReducer: taskReducer.reducer,
+  },
+})
+console.log(store.getState());
+
+
+
+console.log(store.dispatch(addTask("Buy Phone")));
+console.log(store.dispatch(deleteTask(1)));
+console.log(store.getState());
+console.log(store.dispatch(addTask("Buy copy")));
+console.log(store.dispatch(addTask("Buy Car")));
+console.log(store.getState());
 export default initialState;
